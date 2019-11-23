6fcbd8200fc55ff9354617509f3d145729033707
tnoka <mxx.2963@icloud.com>
2019-11-23 22:20:09 +0900

    firstcommit

---
+++ resources/js/app.js
@@ -0,0 +1,24 @@
+import './bootstrap'
+import Vue from 'vue'
+// ルーティングの定義をインポートする
+import router from './router'
+// Vuexのストアをインポートする
+import store from './store'
+// ルートコンポーネントをインポートする
+import App from './App.vue'
+
+const createApp = async () => {
+    await store.dispatch('auth/currentUser')
+
+    new Vue({
+        el: '#app',
+        router,
+        store,
+        components: {
+            App
+        },
+        template: '<App />'
+    })
+}
+
+createApp()
---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// 登録
+Route::post('/register', 'Auth\RegisterController@register')->name('register');
+
+// ログイン
+Route::post('/login', 'Auth\LoginController@login')->name('login');
+
+// ログアウト
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// ログインユーザー
+Route::get('/user', function () {
+    return Auth::user();
+})->name('user');
