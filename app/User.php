dc5b9263aefaccc7841fa48bdb213bb1ab57632f
tnoka <mxx.2963@icloud.com>
2019-11-24 16:56:08 +0900

    add PhotoList

--- app/User.php
+++ app/User.php
@@ -9,7 +9,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
@@ -18,16 +17,14 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password',
     ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
+    /** JSONに含める属性 */
+    protected $visible = [
+        'name',
     ];
-
+    /**
+     * リレーションシップ - photosテーブル
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function photos()
     {
         return $this->hasMany('App\Photo');
